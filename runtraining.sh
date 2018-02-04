3647903a81b70ed043a5d5720793b0263c7c1abd
andrewb-ms <32884624+andrewb-ms@users.noreply.github.com>
2018-02-04 11:09:02 -0800

    Update runtraining.sh

--- runtraining.sh
+++ runtraining.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python style.py --style path/to/style/img.jpg \
+python style.py --style /msshared/inputpaintings/degas1.jpg \
   --checkpoint-dir checkpoint/path \
   --test path/to/test/img.jpg \
   --test-dir path/to/test/dir \
