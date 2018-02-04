6d11a6f64505711cacd57d4aaa323072cd1cf499
andrewb-ms <32884624+andrewb-ms@users.noreply.github.com>
2018-02-03 23:30:08 -0800

    Update server.py

--- server.py
+++ server.py
@@ -4,3 +4,12 @@ app = Flask(__name__)
 @app.route('/')
 def root():
     return 'Server is up'
+
+
+@app.route('/tensorflow_la_muse')
+def tensorflow_la_muse():
+    execfile("python /home/adm101/fast-style-transfer/evaluate.py --checkpoint /msshared/models/fastmodels/la_muse.ckpt --in-path /msshared/tensorflow/ --out-path /msshared/tfoutput/")
+    return 'Server is up'
+
+
+
