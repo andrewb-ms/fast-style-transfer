51c2994ebded267fc3a8508adedfac8e5f705308
andrewb-ms <32884624+andrewb-ms@users.noreply.github.com>
2018-02-04 00:41:17 -0800

    Update server.py

--- server.py
+++ server.py
@@ -120,28 +120,3 @@ def tensorflow_udnie():
             return send_file(os.path.join('/msshared/tfoutput/', filename), mimetype='image/' + os.path.splitext(filename)[1][1:])
 
     return 'Done'
-
-@app.route('/tensorflow/monet', methods=['POST'])
-def tensorflow_monet():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            #flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            #flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            #shutil.rmtree('/msshared/tensorflow/*')
-            files = glob.glob('/msshared/tensorflow/*')
-            for f in files:
-                os.remove(f)
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            os.system('python evaluate.py --checkpoint /msshared/models/fastmodels/multistyle-pastiche-generator-monet.ckpt --in-path /msshared/tensorflow/ --out-path /msshared/tfoutput/')
-            return send_file(os.path.join('/msshared/tfoutput/', filename), mimetype='image/' + os.path.splitext(filename)[1][1:])
-
-    return 'Done'
