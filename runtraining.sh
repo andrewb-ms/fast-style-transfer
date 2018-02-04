f3abe44104358882400e06b56d359f1a3dcd34b8
andrewb-ms <32884624+andrewb-ms@users.noreply.github.com>
2018-02-04 11:07:53 -0800

    Update runtraining.sh

--- runtraining.sh
+++ runtraining.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+python style.py --style path/to/style/img.jpg \
+  --checkpoint-dir checkpoint/path \
+  --test path/to/test/img.jpg \
+  --test-dir path/to/test/dir \
+  --content-weight 1.5e1 \
+  --checkpoint-iterations 1000 \
+  --batch-size 20
