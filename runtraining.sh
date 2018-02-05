db888ed18d5b25f97c9fc93a041444a1d10e30e2
andrewb-ms <32884624+andrewb-ms@users.noreply.github.com>
2018-02-04 18:56:07 -0800

    Update runtraining.sh

--- runtraining.sh
+++ runtraining.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python style.py --style /msshared/inputpaintings/degas1.jpg \
+nohup python style.py --style /msshared/inputpaintings/degas1.jpg \
   --checkpoint-dir /msshared/trainingtest_newmodels/ \
   --test /msshared/trainingtest_input/test3.jpg \
   --test-dir /msshared/trainingtest_output/ \
   --content-weight 1.5e1 \
   --checkpoint-iterations 1000 \
-  --batch-size 20
+  --batch-size 20 &
