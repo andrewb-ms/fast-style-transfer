7f3ef5f225082d0a4019fd658bdebf6ed931270d
andrewb-ms <32884624+andrewb-ms@users.noreply.github.com>
2018-02-08 15:40:02 -0800

    Update builddocker.sh

--- builddocker.sh
+++ builddocker.sh
@@ -1,1 +1,3 @@
-docker build --no-cache .
+az acr login --name mulesoftpm.azurecr.io/fast-style-transfer
+docker build --no-cache . -t mulesoftpm.azurecr.io/fast-style-transfer
+docker push mulesoftpm.azurecr.io/fast-style-transfer
