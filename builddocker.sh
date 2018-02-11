00f4f2546df6d554755d1bf274b7c79b8445c920
andrewb-ms <32884624+andrewb-ms@users.noreply.github.com>
2018-02-10 20:21:53 -0800

    Update builddocker.sh

--- builddocker.sh
+++ builddocker.sh
@@ -1,5 +1,7 @@
 git pull
-cp /msshared . -r
+cp /msshared/models/fastmodels . -r
+cp /msshared/tensorflow . -r
+cp /msshared/tfoutput . -r
 docker rmi mulesoftpm.azurecr.io/fast-style-transfer
 az acr login --name mulesoftpm
 docker build --no-cache . -t mulesoftpm.azurecr.io/fast-style-transfer
