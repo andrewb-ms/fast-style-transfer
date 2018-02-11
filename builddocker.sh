536ae238d72e3692a1384e39e33232bc2a334616
andrewb-ms <32884624+andrewb-ms@users.noreply.github.com>
2018-02-10 16:51:26 -0800

    Update builddocker.sh

--- builddocker.sh
+++ builddocker.sh
@@ -1,4 +1,5 @@
 git pull
+cp /msshared . -r
 docker rmi mulesoftpm.azurecr.io/fast-style-transfer
 az acr login --name mulesoftpm
 docker build --no-cache . -t mulesoftpm.azurecr.io/fast-style-transfer
